b2c9458cce98757ee5be0d94b9f4f0a8e18fc40a
Jason Neel <jason.neel@gmail.com>
2026-01-21 00:22:45 -0800

    Initial Commit

---
+++ NightSkyTextureGenerator/NightSkyTextureGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightSkyTextureGeneratorApp.swift
+//  NightSkyTextureGenerator
+//
+//  Created by Jason Neel on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct NightSkyTextureGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
